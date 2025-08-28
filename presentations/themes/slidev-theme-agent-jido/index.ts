65a2034f855515bae0fda7ad4e1885229b0bd5b7
Mike Hostetler <84222+mikehostetler@users.noreply.github.com>
2025-08-28 17:48:41 -0500

    Montreal Elixir preso prep

---
+++ presentations/themes/slidev-theme-agent-jido/index.ts
@@ -0,0 +1,9 @@
+// Slidev Theme entry
+// Import global styles
+import './styles/index.css'
+
+export default {
+  // theme meta (optional)
+  name: 'Agent Jido',
+  colorSchema: 'dark',
+}
---
+++ presentations/themes/slidev-theme-agent-jido/layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="slidev-layout">
+    <div class="aj-container">
+      <slot />
+    </div>
+  </div>
+</template>
