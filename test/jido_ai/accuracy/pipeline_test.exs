3ecc266375a470a3a016317a8a9f1fe2e90ac2fc
Pascal Charbonneau <pcharbon70@gmail.com>
2026-01-25 11:44:26 -0500

    test: fix unused generator in pipeline_test.exs

--- test/jido_ai/accuracy/pipeline_test.exs
+++ test/jido_ai/accuracy/pipeline_test.exs
@@ -146,7 +146,7 @@ defmodule Jido.AI.Accuracy.PipelineTest do
       assert %PipelineResult{} = result
     end
 
-    test "applies timeout to pipeline execution", %{generator: generator} do
+    test "applies timeout to pipeline execution", %{generator: _generator} do
       # Create a slow generator
       slow_generator = fn _query, _context ->
         Process.sleep(200)
